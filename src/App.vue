33eccb2d7580c8cc2e1305b89956a516fbbb3599
yellhtutaung <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>
2024-01-12 00:41:02 +0630

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+  import Button from 'primevue/button';
+</script>
+
+<template>
+  <div class="flex align-items-center justify-content-center">
+    <div class="card">
+      <h1>56</h1>
+      <Button icon="pi pi-bookmark" severity="secondary" rounded aria-label="Bookmark" />
+    </div>
+
+  </div>
+</template>
+
+<style>
+  body
+  {
+      background: black!important;
+  }
+  h1{
+      font-size: 100px;
+      color: white;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import { createPinia } from 'pinia'
+import App from './App.vue'
+import 'primevue/resources/themes/lara-light-green/theme.css'
+import 'primeflex/primeflex.scss';
+const app = createApp(App)
+app.use(PrimeVue);
+app.use(createPinia())
+
+app.mount('#app')
